08eea915db5e3e5251985739b823d2f22a11c78b
dev-sanghoon <dev.sanghoon@gmail.com>
2022-12-19 16:18:24 +0900

    add lint & formatter

--- index.js
+++ index.js
@@ -7,51 +7,51 @@ const port = 3000;
 app.use(express.json());
 
 app.get('/api', (req, res) => {
-    const THUMB_LOCATION = './blog/thumbs.json';
-    const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
-    const thumbs = JSON.parse(bufferThumbs.toString());
-    res.send(thumbs);
+	const THUMB_LOCATION = './blog/thumbs.json';
+	const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
+	const thumbs = JSON.parse(bufferThumbs.toString());
+	res.send(thumbs);
 });
 
 app.post('/api/log', (req, res) => {
-    const id = makeSimpleMongoId();
-    const { content } = req.body;
-    const blogInfo = {
-        id,
-        uploadTime: getCurrentISOTime() 
-    };
+	const id = makeSimpleMongoId();
+	const { content } = req.body;
+	const blogInfo = {
+		id,
+		uploadTime: getCurrentISOTime()
+	};
 
-    fs.writeFileSync(`./blog/${id}.md`, content, { flag: 'wx' });
+	fs.writeFileSync(`./blog/${id}.md`, content, { flag: 'wx' });
 
-    const THUMB_LOCATION = './blog/thumbs.json';
-    const thumbExists = fs.existsSync(THUMB_LOCATION);
-    if (thumbExists) {
-        const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
-        const thumbs = JSON.parse(bufferThumbs.toString());
-        const newThumbs = thumbs.push(blogInfo);
-        fs.writeFileSync(THUMB_LOCATION, JSON.stringify(newThumbs));
-    } else {
-        fs.writeFileSync(THUMB_LOCATION, JSON.stringify([blogInfo]));
-    }
+	const THUMB_LOCATION = './blog/thumbs.json';
+	const thumbExists = fs.existsSync(THUMB_LOCATION);
+	if (thumbExists) {
+		const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
+		const thumbs = JSON.parse(bufferThumbs.toString());
+		const newThumbs = thumbs.push(blogInfo);
+		fs.writeFileSync(THUMB_LOCATION, JSON.stringify(newThumbs));
+	} else {
+		fs.writeFileSync(THUMB_LOCATION, JSON.stringify([blogInfo]));
+	}
 
-    res.send(blogInfo);
+	res.send(blogInfo);
 
-    function makeSimpleMongoId() {
-        var result = '';
-        var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < 24; i++ ) {
-            result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        return result;
-    }
+	function makeSimpleMongoId() {
+		var result = '';
+		var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		var charactersLength = characters.length;
+		for (var i = 0; i < 24; i++) {
+			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+		}
+		return result;
+	}
 
-    function getCurrentISOTime() {
-        const now = new Date();
-        return now.toISOString();
-    }
-})
+	function getCurrentISOTime() {
+		const now = new Date();
+		return now.toISOString();
+	}
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+	console.log(`Example app listening on port ${port}`);
 });

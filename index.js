e2018eaac24fb141daa13fa28afcd3dde7976861
dev-sanghoon <dev.sanghoon@gmail.com>
2022-12-27 16:37:39 +0900

    refactor & do not save local

--- index.js
+++ index.js
@@ -15,118 +15,122 @@ app.get('/api', (req, res) => {
 	res.send(thumbs);
 });
 
-app.get('/test', async (req, res) => {
-	async function getNouns(text) {
-		return new Promise((resolve, reject) => {
-			mecab.nouns(text, (err, result) => {
-				if (err) {
-					reject();
+async function getTags(text) {
+	return new Promise((resolve, reject) => {
+		mecab.nouns(text, (err, result) => {
+			if (err) {
+				reject();
+			}
+			const dupRemoved = result.reduce((acc, cur) => {
+				if (acc.indexOf(cur) < 0) {
+					acc.push(cur);
+					return acc;
 				}
-				resolve({
-					success: true,
-					data: result
-				});
-			});
+				return acc;
+			}, []);
+			resolve(dupRemoved);
 		});
-	}
-	const response = await getNouns('아버지가방에들어가신다');
-	res.send(response);
-});
-
-app.post('/api/blog', async (req, res) => {
-	const { content } = req.body;
-	const htmlString = marked.parse(content);
-	const jsDom = new JSDOM(htmlString);
-	const { document } = jsDom.window;
-	const titleElem = document.querySelector('h1');
-
-	if (titleElem === null) {
-		console.log('request failed.');
-		res.send({
-			success: false,
-			message: 'There is no title.'
-		});
-		return;
-	}
-
-	const boldElems = document.querySelectorAll('strong');
-	if (boldElems.length === 0) {
-		console.log('request failed.');
-		res.send({
-			success: false,
-			message: 'There is no summaries.'
-		});
-		return;
-	}
-
-	const summaries = Array.from(boldElems).map((elem) => elem.textContent);
-	const title = titleElem.textContent;
-
-	const id = makeSimpleMongoId();
-
-	async function getTags(text) {
-		return new Promise((resolve, reject) => {
-			mecab.nouns(text, (err, result) => {
-				if (err) {
-					reject();
-				}
-				resolve(result);
-			});
-		});
-	}
-
-	const joinSentence = [title, ...summaries].join(' ');
-	const rawTags = await getTags(joinSentence);
-	const dupRemovedTags = rawTags.reduce((acc, cur) => {
-		if (acc.indexOf(cur) < 0) {
-			acc.push(cur);
-			return acc;
-		}
-		return acc;
-	}, []);
-
-	const blogInfo = {
-		id,
-		attribute: {
-			uploadTime: getCurrentISOTime(),
-			title,
-			summaries,
-			tags: dupRemovedTags
-		}
-	};
-
-	fs.writeFileSync(`./blog/${id}.md`, content, { flag: 'wx' });
-
-	const THUMB_LOCATION = './blog/thumbs.json';
-	const thumbExists = fs.existsSync(THUMB_LOCATION);
-	if (thumbExists) {
-		const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
-		const thumbs = JSON.parse(bufferThumbs.toString());
-		fs.writeFileSync(THUMB_LOCATION, JSON.stringify([...thumbs, blogInfo]));
-	} else {
-		fs.writeFileSync(THUMB_LOCATION, JSON.stringify([blogInfo]));
-	}
-
-	res.send({
-		success: true,
-		message: htmlString
 	});
+}
 
-	function makeSimpleMongoId() {
-		var result = '';
-		var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-		var charactersLength = characters.length;
-		for (var i = 0; i < 24; i++) {
-			result += characters.charAt(Math.floor(Math.random() * charactersLength));
-		}
+function getCurrentISOTime() {
+	const now = new Date();
+	return now.toISOString();
+}
+
+function refinePostBlog(reqBody) {
+	const result = { verified: false };
+
+	if (!reqBody) {
 		return result;
 	}
 
-	function getCurrentISOTime() {
-		const now = new Date();
-		return now.toISOString();
+	if (!reqBody.content) {
+		return result;
 	}
+
+	const { content } = reqBody;
+	Object.assign(result, { content });
+
+	const htmlString = marked.parse(content);
+	const jsDom = new JSDOM(htmlString);
+	const { document } = jsDom.window;
+
+	const titleElem = document.querySelector('h1');
+	if (titleElem === null) {
+		return result;
+	}
+
+	const title = titleElem.textContent;
+	if (!title) {
+		return result;
+	}
+	Object.assign(result, { title });
+
+	const boldElems = document.querySelectorAll('strong');
+	console.log(htmlString);
+	if (boldElems.length === 0) {
+		return result;
+	}
+
+	const summaries = Array.from(boldElems).map((elem) => elem.textContent);
+	const wrongSummaryItems = summaries.filter((item) => !item);
+	if (wrongSummaryItems.length) {
+		return result;
+	}
+	Object.assign(result, { summaries });
+
+	result.verified = true;
+	return result;
+}
+
+app.post('/api/blog', async (req, res) => {
+	const response = { success: false };
+
+	const { verified, ...saveTargets } = refinePostBlog(req.body);
+	if (!verified) {
+		res.send(response);
+		return;
+	}
+
+	const { title, summaries } = saveTargets;
+	const tags = await getTags([title, ...summaries].join(', '));
+	const uploadTime = getCurrentISOTime();
+
+	console.log('expected save:', { ...saveTargets, tags, uploadTime });
+
+	response.success = true;
+	res.send(response);
 });
+
+// function saveLocal(content) {
+// 	const THUMB_LOCATION = './blog/thumbs.json';
+
+// 	const id = makeSimpleMongoId();
+// 	const blogInfo = {
+// 		id,
+// 		uploadTime: getCurrentISOTime()
+// 	};
+// 	fs.writeFileSync(`./blog/${id}.md`, content, { flag: 'wx' });
+// 	const thumbExists = fs.existsSync(THUMB_LOCATION);
+// 	if (thumbExists) {
+// 		const bufferThumbs = fs.readFileSync(THUMB_LOCATION);
+// 		const thumbs = JSON.parse(bufferThumbs.toString());
+// 		fs.writeFileSync(THUMB_LOCATION, JSON.stringify([...thumbs, blogInfo]));
+// 	} else {
+// 		fs.writeFileSync(THUMB_LOCATION, JSON.stringify([blogInfo]));
+// 	}
+
+// 	function makeSimpleMongoId() {
+// 		var result = '';
+// 		var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+// 		var charactersLength = characters.length;
+// 		for (var i = 0; i < 24; i++) {
+// 			result += characters.charAt(Math.floor(Math.random() * charactersLength));
+// 		}
+// 		return result;
+// 	}
+// }
 
 const port = 3000;
 app.listen(port, () => {

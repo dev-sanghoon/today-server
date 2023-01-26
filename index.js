248e650a789071b55ac30a4400b11275110c5a5d
dev-sanghoon <dev.sanghoon@gmail.com>
2023-01-26 18:10:13 +0900

    fix: rollback dotenv

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/test');
 
+require('dotenv').config();
 const isProduction = process.env.NODE_ENV === 'production';
 
 if (!isProduction) {
